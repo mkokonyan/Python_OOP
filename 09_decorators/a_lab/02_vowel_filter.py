8cff62a0607a9bd92b5d8932c0c52e80b1d79328
mkokonyan <mkokonyan@gmail.com>
2021-12-15 23:27:57 +0200

    last commit

---
+++ 09_decorators/a_lab/02_vowel_filter.py
@@ -0,0 +1,13 @@
+def vowel_filter(function):
+    def wrapper():
+        letters = function()
+        return [letter for letter in letters if letter.lower() in "ayouie"]
+    return wrapper
+
+
+@vowel_filter
+def get_letters():
+    return ["a", "b", "c", "d", "e"]
+
+
+print(get_letters())
