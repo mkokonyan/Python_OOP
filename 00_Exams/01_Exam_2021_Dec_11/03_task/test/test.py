bae1c2ad686ce5be418aade97f91a5d92b1a7be3
mkokonyan <mkokonyan@gmail.com>
2021-12-24 00:04:50 +0200

    last commit

--- 00_Exams/01_Exam_2021_Dec_11/03_task/test/test.py
+++ 00_Exams/01_Exam_2021_Dec_11/03_task/test/test.py
@@ -1,5 +1,6 @@
-from project.team import Team
 from unittest import TestCase, main
+
+from project.team import Team
 
 
 class TestTeam(TestCase):
@@ -10,33 +11,33 @@ class TestTeam(TestCase):
         self.assertEqual("Chelsea", self.team.name)
         self.assertEqual({}, self.team.members)
 
-    def test_get_name_with_numbers_raises(self):
+    def test_name_setter_invalid_name_raises(self):
         with self.assertRaises(ValueError) as ex:
-            team = Team("Chelsea_1")
-        self.assertEqual("Team Name can contain only letters!", str(ex.exception))
+            team = Team("Chelsea1")
+            self.assertEqual("Team Name can contain only letters!", str(ex.exception))
 
-    def test_add_member_successfully(self):
-        team = self.team.add_member(Lukaku=29, Werner=26)
-        self.assertEqual({"Lukaku": 29, "Werner": 26}, self.team.members)
-        self.assertEqual("Successfully added: Lukaku, Werner", team)
-
-    def test_add_members_with_repeated_member_name(self):
-        self.team.add_member(Lukaku=29, Werner=26)
-        self.assertEqual({"Lukaku": 29, "Werner": 26}, self.team.members)
-        team = self.team.add_member(Lukaku=32, Pulisic=24, Kepa=22)
-        self.assertEqual({"Lukaku": 29, "Werner": 26, "Pulisic": 24, "Kepa": 22}, self.team.members)
-        self.assertEqual("Successfully added: Pulisic, Kepa", team)
+    def test_add_member(self):
+        trigger = self.team.add_member(Lukaku=31, Werner=26, Kepa=24)
+        self.assertEqual("Successfully added: Lukaku, Werner, Kepa", trigger)
+        self.assertEqual({"Lukaku": 31, "Werner": 26, "Kepa": 24
+                          }, self.team.members)
+        trigger_2 = self.team.add_member(Lukaku=34)
+        self.assertEqual({"Lukaku": 31, "Werner": 26, "Kepa": 24
+                          }, self.team.members)
+        self.assertEqual("Successfully added: ", trigger_2)
 
     def test_remove_member_not_existing_member(self):
         self.team.add_member(Lukaku=29, Werner=26)
+        self.assertEqual({"Lukaku": 29, "Werner": 26,
+                          }, self.team.members)
         self.assertEqual("Member with name Kante does not exist", self.team.remove_member("Kante"))
         self.assertEqual({"Lukaku": 29, "Werner": 26}, self.team.members)
 
     def test_remove_member_successfully(self):
         self.team.add_member(Lukaku=29, Werner=26)
         removed_member = self.team.remove_member("Lukaku")
-        self.assertEqual({"Werner": 26}, self.team.members)
         self.assertEqual("Member Lukaku removed", removed_member)
+        self.assertEqual({"Werner": 26}, self.team.members)
 
     def test_greater_than_members_true_result(self):
         self.team.add_member(Lukaku=29, Werner=26)
@@ -44,7 +45,7 @@ class TestTeam(TestCase):
         team_2 = Team("Liverpool")
         team_2.add_member(Firmino=30)
         self.assertEqual({"Firmino": 30}, team_2.members)
-        self.assertTrue(self.team > team_2)
+        self.assertEqual(True, self.team > team_2)
 
     def test_greater_than_members_false_result(self):
         self.team.add_member(Lukaku=29, Werner=26)
@@ -52,22 +53,22 @@ class TestTeam(TestCase):
         team_2 = Team("Liverpool")
         team_2.add_member(Firmino=30, Salah=29, Henderson=31)
         self.assertEqual({"Firmino": 30, "Salah": 29, "Henderson": 31}, team_2.members)
-        self.assertFalse(self.team > team_2)
+        self.assertEqual(False, self.team > team_2)
 
     def test_len_method(self):
         self.team.add_member(Lukaku=29, Werner=26)
         self.assertEqual(2, len(self.team))
 
     def test_add_method(self):
-        self.team.add_member(Lukaku=29, Werner=26)
+        self.team.members = {"Lukaku": 29, "Werner": 26}
         self.assertEqual({"Lukaku": 29, "Werner": 26}, self.team.members)
         team_2 = Team("Liverpool")
-        team_2.add_member(Firmino=30, Salah=29, Henderson=31)
+        team_2.members = {"Firmino": 30, "Salah": 29, "Henderson": 31}
         self.assertEqual({"Firmino": 30, "Salah": 29, "Henderson": 31}, team_2.members)
         new_team = self.team + team_2
         self.assertEqual("ChelseaLiverpool", new_team.name)
         self.assertEqual({"Lukaku": 29, "Werner": 26, "Firmino": 30, "Salah": 29, "Henderson": 31}, new_team.members)
-        self.assertTrue(isinstance(new_team, Team))
+
 
     def test_str_representation(self):
         team = Team("Test")
